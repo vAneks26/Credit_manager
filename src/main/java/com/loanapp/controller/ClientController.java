050e04642589cf1791b9b45b91a4f389eb778812
vaneks <mister.shlykov1999@mail.ru>
2024-10-23 17:30:57 +0300

    3 commit

--- src/main/java/com/loanapp/controller/ClientController.java
+++ src/main/java/com/loanapp/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.loanapp.controller;
 
 
 
+import com.loanapp.model.Client;
+import com.loanapp.model.CreditApplication;
 import com.loanapp.service.ClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,5 +30,15 @@ public class ClientController {
     public String searchClients(@RequestParam("query") String query, Model model) {
         model.addAttribute("clients", clientService.searchClients(query));
         return "client-list";
+    }
+
+    // Форма для создания новой заявки
+    @GetMapping("/new")
+    public String showApplicationForm(Model model) {
+        System.out.println("GET /new вызван");
+        CreditApplication application = new CreditApplication();
+        application.setClient(new Client());
+        model.addAttribute("application", application);
+        return "application-form";
     }
 }
